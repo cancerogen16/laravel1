bde26af931538c07cf6e7b3088b67698729f617d
cancerogen <don544@mail.ru>
2021-05-14 03:58:56 +0500

    Реализовать несколько страниц

--- routes/web.php
+++ routes/web.php
@@ -2,17 +2,18 @@
 
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
 Route::get('/', function () {
-    return view('welcome');
+    return 'Страница приветствия пользователей';
+});
+
+Route::get('/about', function () {
+    return 'Страница с информацией о проекте';
+});
+
+Route::get('/new/{id}', function (int $id) {
+    return 'Страница для вывода одной новости #' . $id;
+});
+
+Route::get('/news', function () {
+    return 'Страница для вывода нескольких новостей';
 });
