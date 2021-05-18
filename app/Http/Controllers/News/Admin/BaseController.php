689a61c24a455c7751b1fcf5264842b41caa395e
erast <don544@mail.ru>
2021-05-19 01:06:56 +0500

    BaseController

---
+++ app/Http/Controllers/News/Admin/BaseController.php
@@ -0,0 +1,9 @@
+<?php
+// корневой контроллер бэкенда
+namespace App\Http\Controllers\News\Admin;
+
+use App\Http\Controllers\News\BaseController as FrontBaseController;
+abstract class BaseController extends FrontBaseController
+{
+    //
+}
---
+++ app/Http/Controllers/News/BaseController.php
@@ -0,0 +1,9 @@
+<?php
+// корневой контроллер фронтенда
+namespace App\Http\Controllers\News;
+
+use App\Http\Controllers\Controller;
+abstract class BaseController extends Controller
+{
+    //
+}
