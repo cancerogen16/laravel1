ee845480b160424ef9f1a767bb44b129c73ab484
erast <don544@mail.ru>
2021-05-19 00:36:38 +0500

    route('welcome')

--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,33 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Common\HomeController;
+use App\Http\Controllers\Common\WelcomeController;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+use App\Http\Controllers\News\PostController;
 
-Route::get('/', function () {
-    return view('welcome');
+
+// b. Страница категорий новостей.
+// c. Страница вывода новостей по конкретной категории.
+
+// e. Страница авторизации.
+// f. Страница добавления новости.
+
+Auth::routes();
+
+// Главная страница
+Route::get('/', [HomeController::class, 'index'])->name('home');
+
+// a. Страница приветствия.
+Route::get('/welcome', function () {
+    return view('common.welcome');
+})->name('welcome');
+
+Route::get('/news', [PostController::class, 'index']);
+
+// d. Страница вывода конкретной новости.
+Route::get('/news/{id}', [PostController::class, 'show']);
+
+Route::group(['namespace' => 'App\Http\Controllers\News', 'prefix' => 'news'], function () {
+    Route::resource('posts', 'PostController')->names('news.posts');
 });
