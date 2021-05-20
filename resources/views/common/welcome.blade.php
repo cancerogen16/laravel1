2005039b053422f787f95d9e3975a884f161e690
erast <don544@mail.ru>
2021-05-20 23:57:45 +0500

    Страница приветствия

--- resources/views/common/welcome.blade.php
+++ resources/views/common/welcome.blade.php
@@ -9,13 +9,7 @@
 <div class="container">
     <div class="row">
         <div class="col-12">
-            <div class="card">
-                <div class="card-header"><h1>Страница приветствия</h1></div>
-                
-                <div class="card-body">
-                    <h4>Laravel. Глубокое погружение</h4>
-                </div>
-            </div>
+            
         </div>
     </div>
 </div>
