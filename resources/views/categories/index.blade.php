ac0bbc6b3a4360fd5f1899e3ac0f459e83540e74
erast <don544@mail.ru>
2021-05-21 12:09:59 +0500

    Список категорий новостей

--- resources/views/categories/index.blade.php
+++ resources/views/categories/index.blade.php
@@ -3,34 +3,40 @@
 @section('title', 'Список категорий новостей')
 
 @section('content')
-<div class="container py-5">
-    <div class="row">
-        <div class="col-12">
-            <div class="card">
-                <div class="card-header"><h1>Вывод списка категорий новостей</h1> </div>
+    <section class="py-5 text-center container">
+        <div class="row py-lg-5">
+            <div class="col-lg-6 col-md-8 mx-auto">
+                <h1 class="fw-light">Список категорий новостей</h1>
+            </div>
+        </div>
+    </section>
+    <div class="album py-5 bg-light">
+        <div class="container">
+            <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
+                @foreach($categories as $category)
+                    <div class="col">
+                        <div class="card shadow-sm">
+                            <svg class="bd-placeholder-img card-img-top" width="100%" height="225"
+                                 xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: {{ $category['title'] }}"
+                                 preserveAspectRatio="xMidYMid slice" focusable="false">
+                                <title>{{ $category['title'] }}</title>
+                                <rect width="100%" height="100%" fill="#55595c"></rect><text x="50%" y="50%" fill="#eceeef"
+                                                                                             dy=".3em">{{ $category['title'] }}</text>
+                            </svg>
 
-                <div class="card-body">
-                    <table class="table table-striped">
-                        <thead class="thead-dark">
-                            <tr>
-                                <th>ID</th>
-                                <th>Название</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                        @foreach($categories as $category)
-                            <tr>
-                                <td>{{ $category['category_id'] }}</td>
-                                <td><a href="/categories/{{ $category['category_id'] }}/news">{{ $category['title'] }}</a></td>
-                            </tr>
-                        @endforeach
-                        </tbody>
-                    </table>
-                </div>
+                            <div class="card-body">
+                                <div class="d-flex justify-content-between align-items-center">
+                                    <div class="btn-group">
+                                        <a href="/categories/{{ $category['category_id'] }}/news" class="btn btn-sm btn-outline-secondary">Новости из категории</a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                @endforeach
             </div>
         </div>
     </div>
-</div>
 @endsection
 
 
