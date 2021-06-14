b7088f0bd1350cc8def7d1ab0e856a2d00400ccd
cancerogen <don544@mail.ru>
2021-06-14 15:12:35 +0500

    Выйти

--- resources/views/components/admin/header.blade.php
+++ resources/views/components/admin/header.blade.php
@@ -5,10 +5,10 @@
             aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
     </button>
-    <input class="form-control form-control-dark w-100" type="text" placeholder="Search" aria-label="Search">
+    <input class="form-control form-control-dark w-100" type="text" placeholder="Поиск" aria-label="Поиск">
     <ul class="navbar-nav px-3">
         <li class="nav-item text-nowrap">
-            <a class="nav-link" href="#">Sign out</a>
+            <a class="nav-link" href="{{ route('account.logout') }}">Выйти</a>
         </li>
     </ul>
 </header>
